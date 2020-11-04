eeca6c6ffa22401475131388248722bd043c393f
Maxwell Huenink <maxh76@users.noreply.github.com>
2020-11-04 13:38:25 -0600

    Comment code, rename source files

--- SimpleWebClient/WebClient.cs
+++ SimpleWebClient/WebClient.cs
@@ -9,33 +9,43 @@ using System.Runtime.CompilerServices;
 
 namespace SimpleWebClient
 {
-    class Program
+    class WebClient
     {
-        static int requestCounter;
+        static int successfulRequests;
         private static string[] programArguments;
         static async Task Main(string[] args)
         {
             programArguments = args;
             var tasks = new List<Task>();
 
+            // Gets the address from argument list or console
             var address = GetArgumentIfExist(0);
             if(address is null)
             {
-                Console.WriteLine("Enter a host address: ");
+                Console.Write("Enter a host address: ");
                 address = Console.ReadLine();
             }
 
+            // Gets the port from argument list or console
             if (!int.TryParse(GetArgumentIfExist(1), out int port))
             {
                 do
                 {
-                    Console.Write("Enter a port number: ");
+                    Console.Write("\nEnter a port number: ");
                 }
                 while (!int.TryParse(Console.ReadLine(), out port));
             }
 
-            var fileName = GetArgumentIfExist(2) ?? string.Empty;
+            // Gets the fileName from argument list or console
+            var fileName = GetArgumentIfExist(2);
+            if(fileName is null)
+            {
+                Console.Write("\nComplete the GET request\nGET /");
+                fileName = Console.ReadLine();
+            }
 
+            // Gets the number of attempts from argument list
+            // Defaults to 1
             if(!int.TryParse(GetArgumentIfExist(3), out int attempts))
             {
                 attempts = 1;
@@ -43,20 +53,26 @@ namespace SimpleWebClient
 
             var requestToSend = $"GET /{fileName}".ToByteArray();
 
-            Console.WriteLine($"Attempting to connect to {address}:{port} and perform the following request {attempts} times");
+            Console.WriteLine($"\nAttempting to connect to {address}:{port} and perform the following request {attempts} times");
             Console.WriteLine(requestToSend.ToOutputString());
 
             for (int i = 0; i < attempts; i++)
             {
+                // Run the client on a new thread
                 tasks.Add(Task.Run(async () =>
                 {
+                    // The client
                     using var tcpClient = new TcpClient(address, port);
+                    // The stream representing the client's IO
                     using var stream = tcpClient.GetStream();
-                    await SendAndReceive(stream, requestToSend);
+                    // Request data and receive a response from the stream
+                    await RequestAndReceive(stream, requestToSend);
                 }));
             }
+
             try
             {
+                // Wait for all clients to comlete
                 await Task.WhenAll(tasks);
             }
             catch (Exception ex)
@@ -65,19 +81,30 @@ namespace SimpleWebClient
             }
             finally
             {
-                Console.WriteLine($"Successfully completed {requestCounter} requests");
+                Console.WriteLine($"Successfully completed {successfulRequests} requests");
             }
         }
 
-        private static async Task SendAndReceive(NetworkStream stream, byte[] sendBuffer)
+        /// <summary>
+        /// Request a file from the server and receive a response
+        /// </summary>
+        /// <param name="stream">The network stream to send/receive data from</param>
+        /// <param name="sendBuffer">The request to send to the server</param>
+        /// <returns></returns>
+        private static async Task RequestAndReceive(NetworkStream stream, byte[] sendBuffer)
         {
             try
             {
+                // Send data to the server
                 await stream.WriteAsync(sendBuffer);
+
+                // Receive data from the server
                 var receivedData = new byte[20000];
                 var bytesReceived = await stream.ReadAsync(receivedData);
+
+                // Output data and safely incremenet successful connection counter
                 Console.WriteLine($"\n{receivedData[..bytesReceived].ToOutputString()}\n");
-                Interlocked.Increment(ref requestCounter);
+                Interlocked.Increment(ref successfulRequests);
             }
             catch (Exception ex)
             {
@@ -89,6 +116,11 @@ namespace SimpleWebClient
             }
         }
 
+        /// <summary>
+        /// Gets the argument from the argument list if it exists
+        /// </summary>
+        /// <param name="index">The index of the argument in the list</param>
+        /// <returns>A string representing the argument, null if it does not exist</returns>
         private static string GetArgumentIfExist(int index)
         {
             string arg = null;
@@ -102,10 +134,18 @@ namespace SimpleWebClient
 
     public static class MyExtensions
     {
+        /// <summary>
+        /// Turns a string into an array of bytes
+        /// </summary>
+        /// <returns>A byte[] representing the string</returns>
         public static byte[] ToByteArray(this string str) =>
             str.ToCharArray().Select(c => (byte)c).ToArray();
+
+        /// <summary>
+        /// Turns an array of bytes into a string
+        /// </summary>
+        /// <returns>A string representing the byte[]</returns>
         public static string ToOutputString(this byte[] arr) =>
             string.Join("", arr.Select(b => (char)b).ToArray());
-
     }
 }
--- SimpleWebServer/WebServer.cs
+++ SimpleWebServer/WebServer.cs
@@ -10,16 +10,18 @@ using System.Runtime.CompilerServices;
 
 namespace SimpleWebServer
 {
-    class Program
+    class WebServer
     {
         private static readonly string webRoot = Path.Combine(Directory.GetCurrentDirectory(), "www");
+        private static readonly Random rnd = new Random();
 
         static async Task Main(string[] args)
         {
             int port = 8888;
-            var ipAddress = new IPAddress(new byte[] { 0, 0, 0, 0 });
+            var ipAddress = IPAddress.Parse("127.0.0.1");
             var endPoint = new IPEndPoint(ipAddress, port);
 
+            // Create, bind, and listen on a socket
             using var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             listener.Bind(endPoint);
             listener.Listen(10);
@@ -30,21 +32,28 @@ namespace SimpleWebServer
             Console.WriteLine("Press Control + D to gracefully exit when there are existing connection requests");
             Console.WriteLine("Press Control + C to forcefully exit");
             Console.WriteLine($"Listening at {ipAddress}:{port}");
-            ConsoleKeyInfo keyInfo = new ConsoleKeyInfo(' ', ConsoleKey.F24, false, false, false);
+            ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
 
+            // Loop until ^D is pressed
             do
             {
+                // Accept the incoming connection on Socket server
                 var server = await listener.AcceptAsync();
-                tasks.Add(Task.Run(async () => await ConnectAndHandle(server, connections++)));
+
+                // Send and receive on the new socket
+                tasks.Add(Task.Run(async () => await ReceiveAndRespond(server, connections++)));
+
+                // Read in a key press, if any
                 if (Console.KeyAvailable)
                 {
                     keyInfo = Console.ReadKey(true);
                 }
             }
-            while (keyInfo.Key == ConsoleKey.F24 || !(keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) && keyInfo.Key.ToString().ToLower() == "d"));
+            while (!(keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control) && keyInfo.Key.ToString().ToLower() == "d"));
 
             try
             {
+                // Wait for all connections to complete
                 await Task.WhenAll(tasks);
             }
             catch(Exception ex)
@@ -57,54 +66,67 @@ namespace SimpleWebServer
             }
         }
 
-        private static async Task ConnectAndHandle(Socket server, int connectionNumber)
+        /// <summary>
+        /// Receives a message from a client, processes the request, and responds accordingly
+        /// </summary>
+        /// <param name="server">The Socket the connection is on</param>
+        /// <param name="connectionNumber">The current connection number (used for output to console)</param>
+        /// <returns>A Task</returns>
+        private static async Task ReceiveAndRespond(Socket server, int connectionNumber)
         {
+            // Read data
             var receivedData = new byte[20000];
             int bytesReceived = await server.ReceiveAsync(receivedData, SocketFlags.None);
 
+            // Parse data
             var stringData = receivedData[..bytesReceived].ToOutputString();
             var data = stringData.Split(" ");
-            Console.WriteLine($"Connection {connectionNumber} Received: {stringData}");
+            var message = $"Connection {connectionNumber} Received: {stringData}";
 
             byte[] sendBuffer;
 
             string myOkMessage = $"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: ";
             var myFilePath = Path.Combine(webRoot, data[1][1..]);
 
+            // If the request matches GET /
             if (data[0] == "GET" && data[1].Equals("/"))
             {
-                var fileList = $"<html>" +
-                    $"<head>" +
-                    $"<title>index</title>" +
-                    $"</head>" +
-                    $"<body>" +
-                    $"{string.Join("", Directory.GetFiles(webRoot).Select(f => $@"<p><a href=""{Path.GetFileName(f)}"">{Path.GetFileName(f)}</a></p>"))}" +
-                    $"</body>";
+                var fileList = $"<html>\n" +
+                    $"<head>\n" +
+                    $"    <title>index</title>\n" +
+                    $"</head>\n" +
+                    $"<body>\n" +
+                    $"{string.Join("\n", Directory.GetFiles(webRoot).Select(f => $@"    <p><a href=""{Path.GetFileName(f)}"">{Path.GetFileName(f)}</a></p>"))}\n" +
+                    $"</body>\n" +
+                    $"</html>\n";
                 myOkMessage += $"{fileList.Length}\r\n\n{fileList}";
                 sendBuffer = myOkMessage.ToByteArray();
             }
+            // If requested file exists
             else if (data[0] == "GET" && File.Exists(myFilePath))
             {
                 var file = File.ReadAllText(myFilePath);
                 myOkMessage += $"{file.Length}\r\n\n{file}";
                 sendBuffer = myOkMessage.ToByteArray();
             }
+            // File does not exist or not GET request, return 404
             else
             {
                 var file = File.ReadAllText(Path.Combine(webRoot, "404.html"));
                 sendBuffer = $"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-length: {file.Length}\r\n\n{file}".ToByteArray();
             }
 
+            // Send reponse
             int bytesSent = await server.SendAsync(sendBuffer, SocketFlags.None);
 
-            string message = $"Connection: {connectionNumber} ";
+            // Check status of send
             if (bytesSent == sendBuffer.Length)
             {
-                message += $"Successfully sent {bytesSent} bytes of data";
+                message += $"\nSuccessfully sent {bytesSent} bytes of data";
             }
             else
             {
-                message += $"Failed to send data";
+                message += $"\nFailed to send data";
             }
             Console.WriteLine(message);
             server.Close();
@@ -114,9 +136,17 @@ namespace SimpleWebServer
 
     public static class MyExtensions
     {
+        /// <summary>
+        /// Turns a string into an array of bytes
+        /// </summary>
+        /// <returns>A byte[] representing the string</returns>
         public static byte[] ToByteArray(this string str) =>
             str.ToCharArray().Select(c => (byte)c).ToArray();
 
+        /// <summary>
+        /// Turns an array of bytes into a string
+        /// </summary>
+        /// <returns>A string representing the byte[]</returns>
         public static string ToOutputString(this byte[] arr) =>
             string.Join("", arr.Select(b => (char)b).ToArray());
     }
